251c9809de2cb9461d34d3bc69fda124e1d7a682
Bigjoe <bigjoefilm0@gmail.com>
2025-11-10 19:45:48 +0100

    invoke say hello program

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,45 @@
 import {
-    Connection,
-    Keypair,
-    LAMPORTS_PER_SOL,
-    PublicKey,
-    Transaction,
-    TransactionInstruction,
-    clusterApiUrl,
-    sendAndConfirmTransaction,
-  } from "@solana/web3.js";
-  import { initializeKeypair } from "@solana-developers/helpers";
-  import dotenv from "dotenv";
+  Connection,
+  Keypair,
+  LAMPORTS_PER_SOL,
+  PublicKey,
+  Transaction,
+  TransactionInstruction,
+  sendAndConfirmTransaction,
+} from "@solana/web3.js";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const programId = new PublicKey("5k7DV92TujuPpXxpGX6BQ38v5Lsg38BaXDxebgephoK4");
+const connection = new Connection((`https://devnet.helius-rpc.com/?api-key=${process.env.ENV_HELIUS_API_KEY}`));
+
+export const sayHello = async (payer: Keypair): Promise<string> => {
+    const tx = new Transaction();
+    const instruction = new TransactionInstruction({
+        keys: [],
+        programId: programId,
+    })
+    tx.add(instruction);
+    return sendAndConfirmTransaction(connection, tx, [payer]);
+}
+
+    try{
+        const payer = Keypair.generate();
+        console.log("Public Key:", payer.publicKey.toBase58());
+        await connection.requestAirdrop(payer.publicKey, LAMPORTS_PER_SOL * 1 );
+        const transactionSignature = await sayHello(payer);
+        console.log(
+            `Transaction: https://explorer.solana.com/tx/${transactionSignature}?cluster=devnet`
+          );
+          console.log("Finished successfully");
+
+    }
+
+    catch (error) {
+        if (error instanceof Error) {
+          throw new Error(`An error occurred: ${error.message}`);
+        } else {
+          throw new Error("An unknown error occurred");
+        }
+    }
